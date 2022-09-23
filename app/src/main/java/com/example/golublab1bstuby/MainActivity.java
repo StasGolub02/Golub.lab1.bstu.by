8900bcc33af34c8f58a463c863d742408b777948
Stasnislav Golub BSTU <stasik.pozitiv228@gmail.com>
2022-09-23 15:19:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/golublab1bstuby/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.golublab1bstuby;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //TODO hello world! LAba 1
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
